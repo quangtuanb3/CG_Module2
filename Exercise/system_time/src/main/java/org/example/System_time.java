1083a2590c3fe3d1ee037a0178b32873e90cb3d4
quangtuanb3 <quangtuanb3@gmail.com>
2023-06-06 14:07:49 +0700

    system_time

---
+++ Exercise/system_time/src/main/java/org/example/System_time.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import java.util.Date;
+
+public class System_time {
+    public static void main(String[] args) {
+        Date date = new Date();
+        System.out.println("Now is: " + date);
+    }
+}
