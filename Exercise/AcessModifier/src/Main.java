8bc476a6c5d7dd8688a2fbf5edfb2049db81c29d
quangtuanb3 <quangtuanb3@gmail.com>
2023-06-11 20:25:10 +0700

    static method

---
+++ Exercise/AcessModifier/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        Student s1 = new Student(111, "Hoang");
+        Student s2 = new Student(222, "Khanh");
+        Student s3 = new Student(222, "Huy");
+        Student.change();
+        s1.display();
+        s2.display();
+        s3.display();
+    }
+}
