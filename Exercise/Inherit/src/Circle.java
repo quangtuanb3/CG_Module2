9a34b32052087c964c0b4863d655e27afd832840
quangtuanb3 <quangtuanb3@gmail.com>
2023-06-14 21:25:13 +0700

    Shape inherit

---
+++ Exercise/Inherit/src/Circle.java
@@ -0,0 +1,36 @@
+public class Circle extends Shape {
+    private double radius = 1.0;
+
+    public Circle(String color, boolean filled) {
+        super(color, filled);
+    }
+
+    public Circle() {
+    }
+
+    public Circle(String color, boolean filled, double radius) {
+        super(color, filled);
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public double getArea() {
+        return Math.pow(radius, 2) * Math.PI;
+    }
+
+    public double getPerimeter() {
+        return radius * 2 * Math.PI;
+    }
+
+    @Override
+    public String toString() {
+        return "A Circle with radius= " + radius + ", which is a subclass of " + super.toString();
+    }
+}
