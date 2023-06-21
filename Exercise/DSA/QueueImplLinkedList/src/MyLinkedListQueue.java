7cb49bb9039946d270a00c7004be5a3d3d4fc77c
quangtuanb3 <quangtuanb3@gmail.com>
2023-06-21 16:02:53 +0700

    queue impl linkded list

---
+++ Exercise/DSA/QueueImplLinkedList/src/MyLinkedListQueue.java
@@ -0,0 +1,29 @@
+public class MyLinkedListQueue {
+    Node head;
+    Node tail;
+
+    MyLinkedListQueue() {
+        head = null;
+        tail = null;
+    }
+
+    void enqueue(int key){
+        Node temp = new Node(key);
+        if (this.tail == null) {
+            this.head = this.tail = temp;
+            return;
+        }
+        this.tail.next = temp;
+        this.tail = temp;
+    }
+    public Node dequeue() {
+        if (this.head == null)
+            return null;
+        Node temp = this.head;
+        this.head = this.head.next;
+        if (this.head == null)
+            this.tail = null;
+        return temp;
+    }
+
+}
