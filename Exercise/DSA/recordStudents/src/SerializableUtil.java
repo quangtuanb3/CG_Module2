d2d5dfb19f6ca2a3910dbb6bd3b4f8ebd4addb54
quangtuanb3 <quangtuanb3@gmail.com>
2023-06-23 10:00:23 +0700

    record students

--- Exercise/DSA/recordStudents/src/SerializableUtil.java
+++ Exercise/DSA/recordStudents/src/SerializableUtil.java
@@ -1,24 +1,37 @@
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SerializableUtil {
     // deserialize to Object from given file
-    public static Object deserialize(String fileName) throws IOException,
+    public static List<Object> deserialize(String fileName) throws IOException,
             ClassNotFoundException {
         FileInputStream fis = new FileInputStream(fileName);
         ObjectInputStream ois = new ObjectInputStream(fis);
-        Object obj = ois.readObject();
+        List<Object> objs = new ArrayList<>();
+        try {
+            while (true) {
+                Object obj = ois.readObject();
+                objs.add(obj);
+            }
+        } catch (Exception e) {
+
+        }
         ois.close();
-        return obj;
+        return objs;
     }
 
     // serialize the given object and save it to file
-    public static void serialize(Object obj, String fileName)
+    public static void serialize(List<Object> obj, String fileName)
             throws IOException {
-        FileOutputStream fos = new FileOutputStream(fileName, true);
+        FileOutputStream fos = new FileOutputStream(fileName);
         ObjectOutputStream oos = new ObjectOutputStream(fos);
-        oos.writeObject(obj);
-
+        for (Object student : obj) {
+            oos.writeObject(student);
+        }
         fos.close();
     }
+
+
 
 }
