645ca346bcc7311e391caa3392ad1cf2af6c90f6
quangtuanb3 <quangtuanb3@gmail.com>
2023-06-26 10:43:09 +0700

    BST tree

---
+++ Exercise/Collection/src/BST/AbstractTree.java
@@ -0,0 +1,7 @@
+package BST;
+
+public abstract class AbstractTree<E> implements Tree<E> {
+    @Override
+    public void inorder() {
+    }
+}
---
+++ Exercise/Collection/src/BST/Tree.java
@@ -0,0 +1,9 @@
+package BST;
+
+public interface Tree<E> {
+    public boolean insert(E e);
+
+    public void inorder();
+
+    public int getSize();
+}
